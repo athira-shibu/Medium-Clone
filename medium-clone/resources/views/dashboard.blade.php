66d847af26f1f151d48ee6ae91792b014f9659e5
athira-shibu <athira.shibu@dibasys.com>
2025-07-20 18:52:33 +0530

    styled little more with tailwind

--- medium-clone/resources/views/dashboard.blade.php
+++ medium-clone/resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div>
-        <div class="py-4 bg-yellow-100">
+    <div class="py-4">
+        <div class="">
             <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
                 <li class="me-2">
                     <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">All</a>
@@ -14,18 +14,16 @@
                 @endforeach
             </ul>
         </div>
-        <div class="py-4 bg-green-100 mt-12">
+        <div class="py-6 px-6 bg-green-100 mt-12 max-w-3xl mx-auto rounded-lg">
             <!-- <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center"> -->
                 @foreach ($posts as $post)
-                    <div class="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
-                        </a>
-                        <div class="p-5">
+                    <div class="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex mb-8">
+                        
+                        <div class="p-5 flex-1">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$post->title}}</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$post->content}}</div>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Read more
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -33,6 +31,9 @@
                                 </svg>
                             </a>
                         </div>
+                        <a href="#">
+                            <img class="h-full w-36 rounded-r-lg object-cover" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                        </a>
                     </div>
                 @endforeach
             <!-- </ul> -->
