38d0ddbad4956e1c06e4c5e830c832bd9879ec90
athira-shibu <athira.shibu@dibasys.com>
2025-07-21 21:31:40 +0530

    moved code to component

--- medium-clone/resources/views/dashboard.blade.php
+++ medium-clone/resources/views/dashboard.blade.php
@@ -17,24 +17,7 @@
         <div class="py-6 px-6 bg-green-100 mt-12 max-w-3xl mx-auto rounded-lg">
             <!-- <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center"> -->
                 @forelse ($posts as $post)
-                    <div class="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex mb-8">
-                        
-                        <div class="p-5 flex-1">
-                            <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$post->title}}</h5>
-                            </a>
-                            <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{Str::words($post->content, 15)}}</div>
-                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                            </a>
-                        </div>
-                        <a href="#">
-                            <img class="h-full w-36 rounded-r-lg object-cover" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
-                        </a>
-                    </div>
+                    <x-post-item :post="$post"></x-post-item>
                 @empty
                     <div class="text-center text-gray-400 py-16">No posts found</div>
                 @endforelse
