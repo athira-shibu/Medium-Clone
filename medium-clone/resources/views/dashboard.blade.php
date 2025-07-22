36619bb7f27752a861fcac96cb73be0825e9b359
athira-shibu <athira.shibu@dibasys.com>
2025-07-22 21:28:43 +0530

    created component for categories

--- medium-clone/resources/views/dashboard.blade.php
+++ medium-clone/resources/views/dashboard.blade.php
@@ -1,18 +1,7 @@
 <x-app-layout>
     <div class="py-4">
         <div class="">
-            <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
-                <li class="me-2">
-                    <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">All</a>
-                </li>
-                @foreach ($categories as $category)
-                    <li class="me-2">
-                        <a href="#" class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
-                            {{ $category->name }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
+            <x-category-tabs />
         </div>
         <div class="py-6 px-6 bg-green-100 mt-12 max-w-3xl mx-auto rounded-lg">
             <!-- <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center"> -->
