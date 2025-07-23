e08a83ebbcc303c6d60b3cf133c21fe8a914c646
athira-shibu <athira.shibu@dibasys.com>
2025-07-23 21:39:31 +0530

    changed all to home page itself

--- medium-clone/routes/web.php
+++ medium-clone/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard' , [PostController::class, 'index'])
+Route::get('/' , [PostController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
